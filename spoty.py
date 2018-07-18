d82c6d8148219ef423c65500a5e3ac91cc33bb30
ninja15 <marius.toader15@gmail.com>
2018-07-18 17:23:32 +0100

    first repo spoty file

---
+++ spoty.py
@@ -0,0 +1,14 @@
+playlist = {"title": "Patagonia Bus",
+ 	"author": "Colt Steele",
+ 	"songs": [
+ 		{"title": "song1", "artist": ["blue"], "duration": 2.35},
+ 		{"title": "song2", "artist": ["kitty", "dj kat"], "duration": 2.55},
+ 		{"title": "miau", "artist": ["garfield"], "duration": 2.00}
+	]
+}
+
+total_length = 0
+for song in playlist["songs"]:
+	total_length += song["duration"]
+
+print(total_length)
